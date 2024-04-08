6eced49da07eea5e5577b8c303b2564360973222
huzaifamalik0 <163676761+huzaifamalik0@users.noreply.github.com>
2024-04-08 16:59:45 +0500

    Add files via upload
    adding a number guessing game

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("\n\t Welcome To Huzaifa - CLI-NUMBER-GUESSING-GAME\n");
+
+const randomNumber = Math.floor(Math.random() * 5 + 1 );
+
+ const answer = await inquirer.prompt([
+    {
+        name : "userGuessNumber",
+        type : "number",
+        message : "enter your number(number limit from 1 to 5):",
+        },
+ ]);
+
+ if(answer.userGuessNumber === randomNumber){
+    console.log(" congratulations ! you guess a correct number.");
+ }
+else{
+    console.log(" sorry , you guess a wrong number");
+}
